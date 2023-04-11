1d5c4643938a8cee44f141e8f3a6f72412874abd
Arun kumar Ram <31740570+karun88045@users.noreply.github.com>
2023-04-11 14:59:24 +0530

    Update example13.rb
    ex13 update

--- example13.rb
+++ example13.rb
@@ -9,23 +9,23 @@
 
 class Instagram
   def no_of_test_cases
-	puts "Enter no_of_test_cases"
-	gets.chomp.to_i
+    puts "Enter no_of_test_cases"
+    gets.chomp.to_i
   end
   def user_input
-	puts "Enter No of X and Y — the following and follower count:"
-	@x = gets.chomp.to_i
-	@y = gets.chomp.to_i
+    puts "Enter No of X and Y — the following and follower count:"
+    @x = gets.chomp.to_i
+    @y = gets.chomp.to_i
   end
   def output
-	no_of_test_cases.times do
-	  user_input()
-	  if @x>@y*10
+    no_of_test_cases.times do
+      user_input()
+      if @x>@y*10
         puts("YES")
       else
         puts("NO")
       end
-	end
+    end
   end
 end
 Instagram.new.output()
